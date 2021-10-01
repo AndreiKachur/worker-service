7f765ec90b4a87d7dd1db26b915b99150df59914
Sideborne <Sideborne@bk.ru>
2021-10-01 13:09:52 +0300

    added sample Android

--- android/app/src/main/java/com/workservice/MainActivity.java
+++ android/app/src/main/java/com/workservice/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testapp;
+package com.workservice;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "testApp";
+    return "workService";
   }
 }
