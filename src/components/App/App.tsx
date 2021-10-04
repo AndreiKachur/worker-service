54648baa2fa7a69ad3cab1d1421b9d9c27afd0b9
MikheevMaxim <presswallperm@yandex.ru>
2021-10-04 21:34:42 +0500

    added new pattern React Native

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,46 +1,14 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { View, Text, Button } from 'react-native'
-import StartScreen from '../../screens/StartScreen'
-import AuthScreen from '../../screens/AuthScreen/AuthScreen'
-import MainScreen from '../../screens/MainScreen'
-import NewsScreen from '../../screens/NewsScreen'
-import ServiceScreen from '../../screens/ServiceScreen'
-import EditPrifileScreen from '../../screens/EditProfileScreen'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 import styles from './App.styles';
 
-type AppProps = any;
+type AppProps = {};
 
-const Stack = createStackNavigator()
-
-const App: React.FC<AppProps> = (props) => {
-  
-  return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen 
-        name="Start" 
-        component={StartScreen} 
-        options={({ navigation }) => ({
-          headerRight: () => (
-            <Button
-              onPress={() => navigation.navigate('Auth')}
-              title="Auth"
-            
-            /> )
-        })}
-        />
-        <Stack.Screen name="Auth" component={AuthScreen} />
-        <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="News" component={NewsScreen} />
-        <Stack.Screen name="Services" component={ServiceScreen} />
-        <Stack.Screen name="EditProfile" component={EditPrifileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>  
-  ) 
-} 
-
+const App: React.FC<AppProps> = (props) => (
+  <View style={styles.component}>
+    <Text>New React Native App</Text>
+  </View>
+);
 
 export default App;
