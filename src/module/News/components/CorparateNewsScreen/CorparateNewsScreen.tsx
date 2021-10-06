4110cf8252cb76f700043c57c6e4f706f4566c89
MikheevMaxim <presswallperm@yandex.ru>
2021-10-06 21:04:41 +0500

    Installed empty functions on the buttons(module: News)

--- src/module/News/components/CorparateNewsScreen/CorparateNewsScreen.tsx
+++ src/module/News/components/CorparateNewsScreen/CorparateNewsScreen.tsx
@@ -1,53 +1,21 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Alert } from 'react-native';
+import { privateNewsData } from '../../../../data'
 
 import styles from './CorparateNewsScreen.styles';
 
 type CorparateNewsScreenProps = any;
 
-const news = [
-  {
-    id: 1,
-    title: 'post 1',
-    text: 'text 1',
-  },
-  {
-    id: 2,
-    title: 'post 2',
-    text: 'text 2',
-  },
-  {
-    id: 3,
-    title: 'post 3',
-    text: 'text 3',
-  },
-  {
-    id: 4,
-    title: 'post 4',
-    text: 'text 4',
-  },
-  {
-    id: 5,
-    title: 'post 5',
-    text: 'text 51',
-  },
-  {
-    id: 6,
-    title: 'post 6',
-    text: 'text 6',
-  },
-
-];
+const DATA = privateNewsData
 
 const CorparateNewsScreen: React.FC<CorparateNewsScreenProps> = ({ navigation }) => (
   <View style={styles.component}>
     <Text>CorparateNewsScreen</Text>
-    {news.map((post) => (
+    {DATA.map((post) => (
       <View key={post.id}>
         <Text>{post.title}</Text>
-        <Text>{post.text}</Text>
         <Button
-          onPress={() => navigation.navigate('Post')}
+          onPress={() => { Alert.alert("Заглушка") }}
           title="Open"
         />
       </View>
