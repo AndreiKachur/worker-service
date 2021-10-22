b821d8b92545d7c8aaf770396fd69ef35b648e52
Andrei <underfil@mail.ru>
2021-10-22 15:21:49 +0300

    Refactoring NavigationPrivate

--- frontend/src/components/componentName/NavigationPrivate/NavigationPrivate.js
+++ frontend/src/components/componentName/NavigationPrivate/NavigationPrivate.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabs from '../../common/BottomTabs';
-import { headerTabs, vacationTabs } from './tabItems';
-import ServiceWorkDaysScreen from '../../screens/ServiceWorkDaysScreen';
-import EditProfileScreen from '../../screens/EditProfileScreen';
-import PieceOfNewsScreen from '../../screens/PieceOfNewsScreen';
+import stackItems from './stackItems'
 import color from '../../../themes';
 
 const headerStyle = {
@@ -19,21 +15,19 @@ const Stack = createStackNavigator();
 function NavigationPrivate() {
   return (
     <Stack.Navigator screenOptions={headerStyle}>
-      <Stack.Screen name="Main" options={{ headerShown: false }}>
-        {() => (<BottomTabs items={headerTabs} />)}
-      </Stack.Screen>
-      <Stack.Screen
-        name="PieceOfNews"
-        component={PieceOfNewsScreen}
-        options={{
-          headerTitle: '',
-        }}
-      />
-      <Stack.Screen name="Мой Отпуск" options={{ headerShown: true }}>
-        {() => (<BottomTabs items={vacationTabs} showHeader={false} />)}
-      </Stack.Screen>
-      <Stack.Screen name="Мой День" component={ServiceWorkDaysScreen} />
-      <Stack.Screen name="Edit" component={EditProfileScreen} />
+      {
+        stackItems.map((item) => {
+          return (
+            <Stack.Screen
+              key={Date.now()}
+              name={item.name}
+              options={item.options}
+            >
+              {item.component}
+            </Stack.Screen>
+          )
+        })
+      }
     </Stack.Navigator>
   );
 }
---
+++ frontend/src/components/componentName/NavigationPrivate/stackItems.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import BottomTabs from '../../common/BottomTabs';
+import { headerTabs, vacationTabs } from './tabItems';
+import ServiceWorkDaysScreen from '../../screens/ServiceWorkDaysScreen';
+import EditProfileScreen from '../../screens/EditProfileScreen';
+import PieceOfNewsScreen from '../../screens/PieceOfNewsScreen';
+
+const stackItems = [
+    {
+        name: 'Main',
+        component: () => (<BottomTabs items={headerTabs} />),
+        options: { headerShown: false, headerTitle: '' }
+    },
+    {
+        name: 'PieceOfNews',
+        component: () => (<PieceOfNewsScreen />),
+        options: { headerTitle: '' }
+    },
+    {
+        name: 'Мой Отпуск',
+        component: () => (<BottomTabs items={vacationTabs} showHeader={false} />),
+        options: { headerShown: true, headerTitle: '' }
+    },
+    {
+        name: 'Мой День',
+        component: () => (<ServiceWorkDaysScreen />),
+        options: { headerShown: true, headerTitle: '' }
+    },
+    {
+        name: 'Edit',
+        component: () => (<EditProfileScreen />),
+        options: { headerTitle: 'Edit' }
+    },
+]
+
+export default stackItems
